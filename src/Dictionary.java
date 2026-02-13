c2caa6ad51b6a2d48d12a233a1b9bdc10126abe4
Sandra Borboa <sborboaluna@gmail.com>
2026-02-13 16:46:46 -0700

    Feat: added selectRandomWords method

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Dictionary {
     HashMap<String, String> translatedWords = new HashMap<>();
@@ -6,10 +6,10 @@ public class Dictionary {
     public Dictionary(){
     // Add words with their translations (Spanish, English)
     translatedWords.put("Sobreescribir", "Override");
-    translatedWords.put("Abstraction", "Abstracción");
+    translatedWords.put("Abstracción", "Abstraction");
     translatedWords.put("Encapsulación", "Encapsulation");
     translatedWords.put("Herencia", "Inheritance");
-    translatedWords.put("Polimorfismo", "Polymorphism"); // Duplicate
+    translatedWords.put("Polimorfismo", "Polymorphism");
     translatedWords.put("Clase", "Class");
     translatedWords.put("Objeto", "Object");
     translatedWords.put("Método", "Method");
@@ -26,4 +26,13 @@ public class Dictionary {
     translatedWords.put("Depuración", "Debugging");
     translatedWords.put("Biblioteca", "Library");
     }
+
+    public List<Map.Entry<String, String>> selectRandomWords(){
+        ArrayList<Map.Entry<String, String>> wordsPairs = new ArrayList<>(translatedWords.entrySet()); // Creates ArrayList using the values of the dictionary
+        Collections.shuffle(wordsPairs); // Randomly rearranges order of ArrayList elements every time the method gets called
+
+        return wordsPairs.subList(0,5); // Returns first five pairs of randomly shuffled list
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
 
+        System.out.println(dictionary.selectRandomWords());
     }
 }
